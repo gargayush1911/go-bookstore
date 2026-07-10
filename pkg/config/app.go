830ec39bf77748cb898f3b2408469c2e72f7020f
gargayush1911 <gargayush1911@gmail.com>
2026-07-10 12:42:55 +0530

    changed the password of mysql

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-    d, err := gorm.Open("mysql", "root:Gargayush@9707@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local")
+    d, err := gorm.Open("mysql", "root:********@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local")
     if err != nil {
         panic(err)
     }
